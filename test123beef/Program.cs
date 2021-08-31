5edab23af2f2b9c7e5069ca4cff8cd6d5dc6085b
Ryan De Souza <ryandman117@gmail.com>
2021-08-31 10:46:52 -0400

    Add project files.

---
+++ test123beef/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool end = false;
+
+            Console.WriteLine("Enter a String between 5 and 10, inclusive. For case switches!");
+
+            while (end == false)
+            {
+
+                string input = Console.ReadLine();
+
+                char[] charArr = input.ToCharArray();
+
+                if (charArr.Length >= 5 && charArr.Length <= 10)
+                {
+                    for (int i = 0; i <= charArr.Length; i++)
+                    {
+                        if (Char.IsUpper(charArr[i]) == true)
+                        {
+                            Char.ToLower(charArr[i]);
+
+                        }
+
+                        else
+                        {
+                            Char.ToUpper(charArr[i]);
+                        }
+                    }
+
+
+                    input = new string(charArr);
+                    Console.WriteLine(input);
+
+                    end = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Please enter a string between 5 and 10 inclusive!");
+                }
+            }
+
+            end = false;
+
+            Console.WriteLine("Enter a String between 8 and 20, inclusive. For reverse strings!");
+
+            while (end == false)
+            {
+
+                string input = Console.ReadLine();
+
+                char[] charArr = input.ToCharArray();
+
+                if (charArr.Length >= 8 && charArr.Length <= 20)
+                {
+                    for (int i = charArr.Length; i >= 0; i--)
+                    {
+
+                    }
+
+
+
+
+                    end = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Please enter a string between 8 and 20 inclusive!");
+                }
+            }
+
+        }
+    }
+}
